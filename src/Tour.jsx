629aa82c66fbe75adecbcd16e6bac7108c687574
fahaddevs <fahadbinfaiz9999@gmail.com>
2022-11-12 14:00:51 +0600

    add read more toggle

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Tour({id, name, info, image, price}) {
+
+  const [readmore, setReadmore] = useState(false);
+
   return (
     <>
       <article key={id} className='mt-10'>
@@ -12,7 +15,17 @@ export default function Tour({id, name, info, image, price}) {
             <h2 className='text-2xl font-medium'>{name}</h2>
             <h3 className='px-6 py-2 rounded-lg bg-violet-600 text-white font-medium inline-block'>${price}</h3>
           </div>
-          <p className='mt-3'>{info}</p>
+          
+            <p className='mt-3'>
+              {readmore? info: `${info.substring(0, 250)}... `}
+              <button 
+                type='button' 
+                onClick={()=>setReadmore(!readmore)} 
+                className={readmore ? 'text-red-600 font-medium' : 'text-green-600 font-medium'}
+                >
+                { readmore ? 'Show Less' : 'Read More' }
+              </button>
+            </p>
           
           <button  type='button' className='py-2 px-4 bg-red-600 text-white rounded-lg w-full mt-3'>Remove Item</button>
         </footer>
